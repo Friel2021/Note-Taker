4184cbf4af228f9f4a5fd76123ac578a22342865
Gregory Friel <87154134+Friel2021@users.noreply.github.com>
2021-08-28 17:47:17 -0600

    updated structure

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-const PORT = process.env.PORT || 3001;
-const express = require('express');
-const app = express();
-const fs = require('fs');
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({
-    extended: true
-}));
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static(__dirname));
 
+//Require routes file
+require('./routes/routes')(app);
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
-
-
-
-
-
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
 
 
 
