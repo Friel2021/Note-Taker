c2fca166c17a4a4c4ad2664a8d5784d9398911c0
Gregory Friel <87154134+Friel2021@users.noreply.github.com>
2021-08-29 17:10:05 -0600

    updated readme with screenshot

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Note-Taker
+
+[Note Taker on Heroku!](https://safe-falls-60671.herokuapp.com//)
+
+This is a note taking application using Express and Node.js. This application has functionality to add, view, and delete notes that are being stored in a db.json file.
+
+This application requires the following NPM modules:
+
+fs
+Express
+Path
+Be sure to install npm packages that aren't already included in node.
+
+The initial page will take you to a welcome/get started page, once you click into get started you can start creating your notes!
+
+There are input boxes for both a note title and note text and a save button that pops up in the upper right hand corner of the page once text has been entered. Once a new note is saved it will then show up by title on the left hand side of the page. There will be a red trash can delete button next to each note so that you can delete them. If you click on the note title itself it will pull that note title and text up to the center of the screen.
+
+![Note Taker Start Screen](public/assets/images/Notetaker1.png)
+
+
+![Note Taker app](public/assets/images/Notetaker2.png)
+
--- routes/routes.js
+++ routes/routes.js
@@ -3,34 +3,31 @@ const path = require('path');
 
 module.exports = app => {
 
-    // Setup notes variable
+    
     fs.readFile("db/db.json","utf8", (err, data) => {
 
         if (err) throw err;
 
         var notes = JSON.parse(data);
 
-        // API ROUTES
-        // ========================================================
-    
-        // Setup the /api/notes get route
+      
+      
         app.get("/api/notes", function(req, res) {
-            // Read the db.json file and return all saved notes as JSON.
+          
             res.json(notes);
         });
 
-        // Setup the /api/notes post route
         app.post("/api/notes", function(req, res) {
-            // Receives a new note, adds it to db.json, then returns the new note
+            
             let newNote = req.body;
             notes.push(newNote);
             updateDb();
             return console.log("Added new note: "+newNote.title);
         });
 
-        // Retrieves a note with specific id
+       
         app.get("/api/notes/:id", function(req,res) {
-            // display json for the notes array indices of the provided id
+          
             res.json(notes[req.params.id]);
         });
 
@@ -41,15 +38,12 @@ module.exports = app => {
             console.log("Deleted note with id "+req.params.id);
         });
 
-        // VIEW ROUTES
-        // ========================================================
-
-        // Display notes.html when /notes is accessed
+     
         app.get('/notes', function(req,res) {
             res.sendFile(path.join(__dirname, "../public/notes.html"));
         });
         
-        // Display index.html when all other routes are accessed
+ 
         app.get('*', function(req,res) {
             res.sendFile(path.join(__dirname, "../public/index.html"));
         });
